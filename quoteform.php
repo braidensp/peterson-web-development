3b526867832b4dab295bd4356d33c0f53d0f47e8
Braiden Peterson <braidensp@me.com>
2018-07-21 10:10:54 -0700

    responsive additions

---
+++ quoteform.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Request a Quote</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+</head>
+<body>
+    <button class="dropbtn"><a href="index.html">Back to home</a></button>
+
+<form class="contact">
+    <br>
+    <input type="text" placeholder="Full Name">
+    <br>
+    <input type="text" placeholder="Email">
+    <br>
+    <input type="text" placeholder="Phone Number (optional)">
+    <br>
+    <button name="submit">submit</button>
+</form>
+
+
+
+<?php
+    
+?>
+</body>
+</html>
